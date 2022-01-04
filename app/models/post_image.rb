88e4bd2286b8407d79c9dbd00ac94a5e399a6e2c
gfdsvvgf <patrick.jj@outlook.jp>
2022-01-04 11:36:06 +0000

    5章終了

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,4 +3,9 @@ class PostImage < ApplicationRecord
   has_many :post_images, dependent: :destroy
   attachment :image
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 end
